6edc208984ec647d55ebb50995d1a10e6af813b9
Mohsen <mohsenmadi@gmail.com>
2024-03-24 17:47:46 -0400

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Chart1Component} from "./charts/chart1/chart1.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Chart1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/charts/chart1/chart1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart1',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart1.component.html',
+  styleUrl: './chart1.component.scss'
+})
+export class Chart1Component {
+
+}
